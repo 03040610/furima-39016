94f2ac487175c50fb3a4e099c8896118297df037
03040610 <yukikobayashi0304@gmail.com>
2023-03-08 19:51:47 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :nick_name,          presence: true
+validates :email,              presence: true, uniqueness: true,       format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+validates :first_name,         presence: true
+validates :last_name,          presence: true
+validates :first_kana_name,    presence: true
+validates :last_kana_name,     presence: true
+validates :birth_day,          presence: true
 end
