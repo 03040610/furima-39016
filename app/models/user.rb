5956303d379033956ef7009954bc02d8932659e5
03040610 <yukikobayashi0304@gmail.com>
2023-03-09 21:36:21 +0900

    修正版２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
 validates :nick_name,          presence: true
 validates :password,           presence: true, length: { minimum: 6 },                  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
-validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龯々]+\z/ }
+validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 validates :first_kana_name,    presence: true, format: { with: /\A[ァ-ヶー]+\z/}
 validates :last_kana_name,     presence: true, format: { with: /\A[ァ-ヶー]+\z/}
 validates :birth_day,          presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nick_name             {'test'}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6)}+{Faker::Internet.password(Lorem.characters(min_alpha: 1))}+{Faker::Internet.password(Lorem.characters(min_numeric: 1))}
     password_confirmation {password}
     first_name            {'あ'}
     last_name             {'い'}
