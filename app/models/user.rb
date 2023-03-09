ab909712f8fd5780cf3266cdcd7555e091f3932c
03040610 <yukikobayashi0304@gmail.com>
2023-03-09 17:13:31 +0900

    完成形

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 validates :nick_name,          presence: true
-validates :email,              presence: true, uniqueness: true,       format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-validates :first_name,         presence: true
-validates :last_name,          presence: true
-validates :first_kana_name,    presence: true
-validates :last_kana_name,     presence: true
+validates :email,              presence: true, uniqueness: true,                        format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+validates :password,           presence: true, length: { minimum: 6 },                  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ }
+validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龯]+\z/ }
+validates :first_kana_name,    presence: true, format: { with: /\A[ァ-ヶー]+\z/}
+validates :last_kana_name,     presence: true, format: { with: /\A[ァ-ヶー]+\z/}
 validates :birth_day,          presence: true
 end
