0ad0cabef2b0259c44aa0853474d761e975fdb1b
03040610 <yukikobayashi0304@gmail.com>
2023-03-10 12:49:05 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :item_name,          presence: true
+  validates :item_column,        presence: true
+  validates :item_category_id,   presence: true
+  validates :item_status_id,     presence: true
+  validates :delivery_charge_id, presence: true
+  validates :prefecture_id,      presence: true
+  validates :item_date_id,       presence: true
+  validates :item_price,         presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, numericality: { only_integer: true }
+  validates :image,              presence: true
+  
+  has_one_attached :image
+  belongs_to :user
 end
