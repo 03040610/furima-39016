cfc9256b388e3598ac0ed814fad9585877e414f1
03040610 <yukikobayashi0304@gmail.com>
2023-03-13 20:43:11 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :update, :show]
+
   def index
     @item = Item.order("created_at DESC")
   end
@@ -45,5 +47,8 @@ class ItemsController < ApplicationController
       if (user_signed_in?) && (current_user.id != @item.user_id)
         redirect_to action: :index
       end
+    end
+    def set_item
+      @item = Item.find(params[:id])
     end
 end
