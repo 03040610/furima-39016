800137d69fa7b9beb9bcd38cbb2784e8479efc68
03040610 <yukikobayashi0304@gmail.com>
2023-03-13 19:14:39 +0900

    エラー1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: [:edit]
   def index
     @item = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
+  end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   def show
@@ -25,5 +39,11 @@ class ItemsController < ApplicationController
   private 
     def item_params
       params.require(:item).permit(:item_name, :item_column, :item_price, :image, :item_category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :item_date_id).merge(user_id: current_user.id)
+    end
+    def move_to_index
+      @item=Item.find(params[:id])
+      if (user_signed_in?) || (current_user.id != @item.user_id)
+        redirect_to action: :index
+      end
     end
 end
