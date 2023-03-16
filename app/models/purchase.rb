b9b0f04a02c6ebf15f6bb22c667e945da5be60c2
03040610 <yukikobayashi0304@gmail.com>
2023-03-16 21:32:20 +0900

    formオブジェクトの設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,5 @@
 class Purchase < ApplicationRecord
+  has_one :place
+  belongs_to :item
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
-  resources :users, only: :new
-  resources :items
+  resources :items  do
+    resources :purchase_place, only: [:index, :create]  
+  end
 end
