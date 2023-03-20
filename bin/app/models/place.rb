1902ff9f94ce99c49f7fb6965ee310406b6e84c7
03040610 <yukikobayashi0304@gmail.com>
2023-03-20 18:49:44 +0900

    う

--- bin/app/models/place.rb
+++
@@ -1,6 +0,0 @@
-class Place < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :prefecture
-
-  belongs_to :purchase
-end
--- bin/app/models/purchase.rb
+++
@@ -1,5 +0,0 @@
-class Purchase < ApplicationRecord
-  has_one :place
-  belongs_to :item
-  belongs_to :user
-end
